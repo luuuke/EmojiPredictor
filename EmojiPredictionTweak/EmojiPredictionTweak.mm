cd6a304665238bb01b623056a903dbd4a61895f0
Luke <luke@peng-u-in.de>
2015-07-26 13:49:19 +0200

    Now retains the most prevalent (textual) prediction in the bar while typing

--- EmojiPredictionTweak/EmojiPredictionTweak.mm
+++ EmojiPredictionTweak/EmojiPredictionTweak.mm
@@ -1,4 +1,8 @@
 #line 1 "/Users/ng/Dropbox/EmojiPredictionTweak/EmojiPredictionTweak/EmojiPredictionTweak.xm"
+#import <objc/runtime.h>
+
+#define kNumberOfCandidates [objc_getClass("UIKeyboardPredictionView") numberOfCandidates]
+
 @class TIAutocorrectionList;
 
 @interface TIKeyboardCandidateSingle : NSObject
@@ -16,30 +20,43 @@
 -(NSString*)emojiString;
 @end
 
+@interface UIKeyboardPredictionView : NSObject
++(NSInteger)numberOfCandidates;
+-(void)_setPredictions:(NSArray*)predictions autocorrection:(TIAutocorrectionList*)autocorrection;
+@end
+
 #include <logos/logos.h>
 #include <substrate.h>
-@class UIKeyboardEmojiCategory; @class TIZephyrCandidate; @class UIKeyboardPredictionView; 
-static void (*_logos_orig$_ungrouped$UIKeyboardPredictionView$_setPredictions$autocorrection$)(UIKeyboardPredictionView*, SEL, NSArray *, TIAutocorrectionList *); static void _logos_method$_ungrouped$UIKeyboardPredictionView$_setPredictions$autocorrection$(UIKeyboardPredictionView*, SEL, NSArray *, TIAutocorrectionList *); 
-static __inline__ __attribute__((always_inline)) Class _logos_static_class_lookup$TIZephyrCandidate(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("TIZephyrCandidate"); } return _klass; }static __inline__ __attribute__((always_inline)) Class _logos_static_class_lookup$UIKeyboardEmojiCategory(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("UIKeyboardEmojiCategory"); } return _klass; }
-#line 18 "/Users/ng/Dropbox/EmojiPredictionTweak/EmojiPredictionTweak/EmojiPredictionTweak.xm"
+@class UIKeyboardEmojiCategory; @class UIKeyboardPredictionView; @class TIZephyrCandidate; 
+static void (*_logos_orig$_ungrouped$UIKeyboardPredictionView$_setPredictions$autocorrection$)(UIKeyboardPredictionView*, SEL, NSArray*, TIAutocorrectionList*); static void _logos_method$_ungrouped$UIKeyboardPredictionView$_setPredictions$autocorrection$(UIKeyboardPredictionView*, SEL, NSArray*, TIAutocorrectionList*); 
+static __inline__ __attribute__((always_inline)) Class _logos_static_class_lookup$UIKeyboardEmojiCategory(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("UIKeyboardEmojiCategory"); } return _klass; }static __inline__ __attribute__((always_inline)) Class _logos_static_class_lookup$TIZephyrCandidate(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("TIZephyrCandidate"); } return _klass; }
+#line 27 "/Users/ng/Dropbox/EmojiPredictionTweak/EmojiPredictionTweak/EmojiPredictionTweak.xm"
 
 
-static void _logos_method$_ungrouped$UIKeyboardPredictionView$_setPredictions$autocorrection$(UIKeyboardPredictionView* self, SEL _cmd, NSArray * predictions, TIAutocorrectionList * autocorrection){
+
+static void _logos_method$_ungrouped$UIKeyboardPredictionView$_setPredictions$autocorrection$(UIKeyboardPredictionView* self, SEL _cmd, NSArray* predictions, TIAutocorrectionList* autocorrection){
 	LWLog(@"autocorrection: %@", autocorrection);
 	LWLog(@"old predictions: %@", predictions);
 	NSMutableArray* replacementPredictions=[NSMutableArray new];
 	NSArray* recentEmojis=[_logos_static_class_lookup$UIKeyboardEmojiCategory() emojiRecentsFromPreferences];
-	for(int i=0; i<15 && i<[recentEmojis count]; i++){
+	
+	for(int i=0; i<kNumberOfCandidates && i<[recentEmojis count]; i++){
+		if(kNumberOfCandidates/2 == i && autocorrection && [predictions count]>0){
+			[replacementPredictions addObject:predictions[0]];
+			
+		}
+		
 		UIKeyboardEmoji* keyboardEmoji=recentEmojis[i];
 		TIZephyrCandidate* emojiCandidate=[_logos_static_class_lookup$TIZephyrCandidate() candidateWithCandidate:keyboardEmoji.emojiString forInput:@""];
 		[replacementPredictions addObject:emojiCandidate];
 	}
-	LWLog(@"new predictions: %@", replacementPredictions);
 	
+	LWLog(@"new predictions: %@", replacementPredictions);
+	[replacementPredictions autorelease];
 	_logos_orig$_ungrouped$UIKeyboardPredictionView$_setPredictions$autocorrection$(self, _cmd, replacementPredictions, NULL);
 }
 
 
 static __attribute__((constructor)) void _logosLocalInit() {
 {Class _logos_class$_ungrouped$UIKeyboardPredictionView = objc_getClass("UIKeyboardPredictionView"); MSHookMessageEx(_logos_class$_ungrouped$UIKeyboardPredictionView, @selector(_setPredictions:autocorrection:), (IMP)&_logos_method$_ungrouped$UIKeyboardPredictionView$_setPredictions$autocorrection$, (IMP*)&_logos_orig$_ungrouped$UIKeyboardPredictionView$_setPredictions$autocorrection$);} }
-#line 36 "/Users/ng/Dropbox/EmojiPredictionTweak/EmojiPredictionTweak/EmojiPredictionTweak.xm"
+#line 53 "/Users/ng/Dropbox/EmojiPredictionTweak/EmojiPredictionTweak/EmojiPredictionTweak.xm"
--- EmojiPredictionTweak/EmojiPredictionTweak.xm
+++ EmojiPredictionTweak/EmojiPredictionTweak.xm
@@ -1,3 +1,7 @@
+#import <objc/runtime.h>
+
+#define kNumberOfCandidates [objc_getClass("UIKeyboardPredictionView") numberOfCandidates]
+
 @class TIAutocorrectionList;
 
 @interface TIKeyboardCandidateSingle : NSObject
@@ -15,19 +19,30 @@
 -(NSString*)emojiString;
 @end
 
+@interface UIKeyboardPredictionView : NSObject
++(NSInteger)numberOfCandidates;
+-(void)_setPredictions:(NSArray*)predictions autocorrection:(TIAutocorrectionList*)autocorrection;
+@end
+
 %hook UIKeyboardPredictionView
 
 //shoutout to PoomSmart @ https://github.com/PoomSmart/MorePredict/
--(void)_setPredictions:(NSArray *)predictions autocorrection:(TIAutocorrectionList *)autocorrection{
+-(void)_setPredictions:(NSArray*)predictions autocorrection:(TIAutocorrectionList*)autocorrection{
 	LWLog(@"autocorrection: %@", autocorrection);
 	LWLog(@"old predictions: %@", predictions);
 	NSMutableArray* replacementPredictions=[NSMutableArray new];
 	NSArray* recentEmojis=[%c(UIKeyboardEmojiCategory) emojiRecentsFromPreferences];
-	for(int i=0; i<15 && i<[recentEmojis count]; i++){
+	
+	for(int i=0; i<kNumberOfCandidates && i<[recentEmojis count]; i++){
+		if(kNumberOfCandidates/2 == i && autocorrection && [predictions count]>0){
+			[replacementPredictions addObject:predictions[0]];
+		}
+		
 		UIKeyboardEmoji* keyboardEmoji=recentEmojis[i];
 		TIZephyrCandidate* emojiCandidate=[%c(TIZephyrCandidate) candidateWithCandidate:keyboardEmoji.emojiString forInput:@""];
 		[replacementPredictions addObject:emojiCandidate];
 	}
+	
 	LWLog(@"new predictions: %@", replacementPredictions);
 	[replacementPredictions autorelease];
 	%orig(replacementPredictions, NULL);
